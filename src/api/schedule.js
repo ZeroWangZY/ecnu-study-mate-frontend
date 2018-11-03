06450ec72573b1a01be3dd7da78208ff6cffbc0e
the little one <542581960@qq.com>
2018-11-03 13:41:40 +0800

    从api.js中拆分出与schedule相关部分

--- src/api/schedule.js
+++ src/api/schedule.js
@@ -20,3 +20,53 @@ export const deleteScheduleReviewAPI = (id) => {
     review_id: id
   })
 }
+
+export const getScheduleAPI = () => {
+    return post('/schedule/search', {
+        eq_studentId: getUserInfo().studentId,
+        limit: 1000
+    }).then(res => res.data.rows)
+}
+
+export const addScheduleAPI = (title, desc, start, end, type, progress) => {
+    return post('/schedule/baseSqlHandle', {
+        insert: [{
+            'studentId': getStudentId(),
+            'startTime': start.replace('T', ':'),
+            'endTime': end.replace('T', ':'),
+            'scheduleType': 'calendar',
+            'title': title,
+            'content': desc,
+            'scheduleTag': type,
+            'progress': progress
+        }],
+        update: [],
+        delete: []
+    });
+}
+
+export const updateScheduleAPI = (id, title, desc, start, end, type, progress) => {
+    return post('/schedule/baseSqlHandle', {
+        insert: [],
+        update: [{
+            'id': id,
+            'studentId': getStudentId(),
+            'startTime': start.replace('T', ':'),
+            'endTime': end.replace('T', ':'),
+            'scheduleType': 'calendar',
+            'title': title,
+            'content': desc,
+            'scheduleTag': type,
+            'progress': progress
+        }],
+        delete: []
+    });
+}
+
+export const deleteScheduleAPI = (id) => {
+    return post('/schedule/baseSqlHandle', {
+        insert: [],
+        update: [],
+        delete: [id]
+    });
+}
