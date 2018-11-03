55424128305f20b695d3120c41d4ce84e923a4fe
the little one <542581960@qq.com>
2018-11-03 13:42:41 +0800

    新建homework.js,并从api.js中拆分出与homework相关部分

---
+++ src/api/homework.js
@@ -0,0 +1,108 @@
+import {post} from "./api";
+import { getStudentId, getAccessToken, getUserInfo , getReceiverId} from "../redux/store";
+
+const urlPrefix = '/api'
+
+export const getHomeworkDetailAPI = () => {
+    let data = {
+        receiver_id: getReceiverId()
+    }
+    return post('/homework/search',data).then(res =>res.data);
+}
+
+export const addHomeworkAPI = (title,content1,deadline,publisher,receiver) => {
+  //  console.log("addHomeworkAPI"+" "+title+" "+content1+" "+deadline+" "+publisher+" "+receiver);
+    return post('/homework/add', {
+            'title':title,
+            'content':content1,
+            'deadline':deadline,
+            'publisher':publisher,
+            'receiver':receiver
+    })
+}
+
+export const deleteHomeworkAPI = (homeworkID) => {
+    return post('/homework/delete',{
+        "homework_id" : homeworkID
+    });
+}
+
+export const updateHomeworkAPI = (homeworkID,title,content1,deadline,receiver,grade,state) =>{
+    console.log("updateHomeworkAPI"+content1);
+    return post("/homework/update",{
+        'title':title,
+        "homework_id" : homeworkID,
+        'content':content1,
+        'deadline':deadline,
+        'grade':grade
+    });
+}
+
+export const downpost = (url, data) => {
+    return fetch(urlPrefix + url, {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + getAccessToken()
+        }
+    }).then(res => res)
+}
+
+export const filePost = (url, data1,homeworkID) => {
+    let data =new FormData();
+    data.append('file',data1);
+    data.append('homeworkID',homeworkID);
+    console.log(data1);
+    let request = new Request(urlPrefix + url, {
+        method: 'POST',
+        mode:'cors',
+        headers: {
+            'Authorization': 'Bearer ' + getAccessToken()
+        },
+        body: data
+    });
+    return fetch(request)
+        .then( res => res.json())
+        .catch((err)=>{
+        console.log("发生了一个错误,存在相同的文件或者上传文件不合法;");
+    });
+}
+
+export const uploadFilesAPI =(data,homeworkID)=>{
+    return filePost('/file/fileupload',data,homeworkID);
+}
+
+export const downFilesAPI =(homeworkID)=>{
+    return downpost('/file/filedownload',{
+        'homeworkID':homeworkID
+    });
+}
+
+export const changeHomeworkContentAPI = (id,content) => {
+    return post('/homework/content',{
+            'content':content,
+            'homework_id':id
+    });
+}
+
+export const changeHomeworkMarkAPI = (id,grade) =>{
+    return post('/homework/mark',{
+        insert:[],
+        update:[{
+            'grade':grade,
+            'homework_id':id
+        }],
+        delete:[]
+    });
+}
+
+export const homeworkFinishAPI = (id) =>{
+    return post('/homework/finish',{
+        insert:[],
+        update:[{
+            'homework_id':id
+        }],
+        delete:[]
+    });
+}
