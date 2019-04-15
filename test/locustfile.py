b676b3c3288e6672a6a0456c22cb5b502ef58cbc
zerowangzy <zerowangzy@zerowangzys-MacBook-Air.local>
2019-04-15 21:14:35 +0800

    edit locustfile

--- test/locustfile.py
+++ test/locustfile.py
@@ -31,19 +31,46 @@ class UserBehavior(TaskSet):
         print(self.id + ' login success, token = '+ token)
 
     @task(1)
-    def index(self):
-        self.client.get("/")
+    def getSchedule(self):
+        response = self.client.post("/api/schedule/search", json = {"eq_studentId": self.id, "limit": 1000}, headers=self.getHeader())
+        print(self.id + " get schedule" + response.content)
+        return json.loads(response.content)["data"]["rows"][0]["id"]
 
     @task(1)
     def getInfo(self):
         response = self.client.post("/api/user/getInfo", json = {"studentId": self.id}, headers = self.getHeader())
-        print(self.id + " getInfo" + response.content)
+        print(self.id + " get info" + response.content)
 
-    # @task(1)
-    # def profile(self):
-    #     self.client.get("/profile")
+    @task(2)
+    def addAndDeleteSchedule(self):
+        response = self.client.post("/api/schedule/baseSqlHandle",
+            json = {
+                "insert": [
+                {
+                    "studentId": self.id,
+                    "startTime": "2019-04-15:16:52",
+                    "endTime": "2019-04-15:17:52",
+                    "scheduleType": 'calendar',
+                    "title": "title",
+                    "content": "desc",
+                }
+                ],
+                "update": [],
+                "delete": []
+            }, headers=self.getHeader())
+        print(self.id + " add schedule" + response.content)
+        id = self.getSchedule()
+        response = self.client.post("/api/schedule/baseSqlHandle", json={
+            "insert": [],
+            "update": [],
+            "delete": [id]
+        }, headers=self.getHeader())
+        print(self.id + " delete schedule" + response.content)
+
+
+
 
 class WebsiteUser(HttpLocust):
     task_set = UserBehavior
-    min_wait = 5000
-    max_wait = 9000
+    min_wait = 1000
+    max_wait = 5000
